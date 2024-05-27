6241de115126a8db15d95af141812b39ca5759a8
Pedro <pedrobesseofi@gmail.com>
2024-05-27 11:29:50 +0200

    HashTagArrayType convert to php and db

--- src/Infrastructure/Persistence/Doctrine/Types/HashTagArrayType.php
+++ src/Infrastructure/Persistence/Doctrine/Types/HashTagArrayType.php
@@ -1,26 +1,34 @@
-<?
+<?php
+
 namespace SocialNetworksPublisher\Infrastructure\Persistence\Doctrine\Types;
+
+use Behat\Behat\Tester\Exception\PendingException;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
+use SocialNetworksPublisher\Domain\Model\Post\HashTagArray;
 
-class HashTagArrayType extends Type {
+class HashTagArrayType extends Type
+{
     public const TYPE_NAME = "hashTagArray";
-    public function getName(): string {
+    public function getName(): string
+    {
         return self::TYPE_NAME;
     }
 
     public function getSQLDeclaration(array $column, AbstractPlatform $platform)
     {
-        
+        return 'text';
     }
-
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    /** @param HashTagArray $value */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
-        
+        return serialize($value);
     }
-
-    public function convertToPHPValue($value, AbstractPlatform $platform)
+    /** @param string $value
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): HashTagArray
     {
-        
+        /** @var HashTagArray */
+        return unserialize($value);
     }
 }
--- tests/unit/Infrastructure/Persistence/Doctrine/Types/HashTagArrayTypeTest.php
+++ tests/unit/Infrastructure/Persistence/Doctrine/Types/HashTagArrayTypeTest.php
@@ -1,11 +1,41 @@
 <?php
+
 namespace SocialNetworksPublisher\Tests\Infrastructure\Persistence\Doctrine\Types;
 
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use PHPUnit\Framework\TestCase;
+use SocialNetworksPublisher\Domain\Model\Post\HashTagArray;
+use SocialNetworksPublisher\Domain\Model\Post\HashTagArrayFactory;
 use SocialNetworksPublisher\Infrastructure\Persistence\Doctrine\Types\HashTagArrayType;
 
-class HashTagArrayTypeTest extends TestCase {
-    public function testGetName(): void {
+class HashTagArrayTypeTest extends TestCase
+{
+    public function testGetName(): void
+    {
         $this->assertEquals('hashTagArray', (new HashTagArrayType())->getName());
+    }
+
+    public function testConvertValue(): void
+    {
+        $type = new HashTagArrayType();
+
+        $dbValue = $type->convertToDatabaseValue(
+            $hashTags = $this->generateHashTagArray(),
+            new SqlitePlatform(),
+        );
+        $phpValue = $type->convertToPHPValue(
+            $dbValue,
+            new SqlitePlatform(),
+        );
+
+        $this->assertIsString($dbValue);
+        $this->assertEquals($hashTags, $phpValue);
+    }
+    
+    public function generateHashTagArray(): HashTagArray
+    {
+        $factory = new HashTagArrayFactory();
+        $hashTags  = $factory->buildHashTagArrayFromSentence("1, #2, 3, , ", ", ");
+        return $hashTags;
     }
 }
