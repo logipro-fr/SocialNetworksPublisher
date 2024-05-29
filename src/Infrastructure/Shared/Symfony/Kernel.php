4ce789dcc334778419e2d0d33c53845a1a1a84e3
Pedro <pedrobesseofi@gmail.com>
2024-05-29 11:16:05 +0200

    Docker config for the swagger

--- src/Infrastructure/Shared/Symfony/Kernel.php
+++ src/Infrastructure/Shared/Symfony/Kernel.php
@@ -11,6 +11,6 @@ class Kernel extends BaseKernel
 
     public function __construct(string $environment, bool $debug)
     {
-        parent::__construct($environment, $debug);
+        parent::__construct($environment, true);
     }
 }
