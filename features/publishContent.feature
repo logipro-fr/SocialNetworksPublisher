81f7048fe129a8bc0994f3a7ed91b4327300af5a
Pedro <pedrobesseofi@gmail.com>
2024-06-03 11:06:40 +0200

    Gurkin change

--- features/publishContent.feature
+++ features/publishContent.feature
@@ -4,20 +4,23 @@ Feature: Publish content on a socialNetworks
   In order to facilitate professional content manipulation on social networks without direct interaction with the user interface
 
   Scenario Outline: Post on social networks 
-    Given I am a <social networks> author
-    And I have written a post for the page "Accident Predict":
+    Given I intend to publish on <social_network>
+    And I have the <social_network> author id "1e3brj"
+    And I have the <social_network> page id "12af56"
+    And I have written a post:
       """
       Following a prediction made at 10:00, an accident occurred on N02 at 10:35. This underscores the importance of accident prediction in prevention.
       """
     And the hashtags are "#AccidentPrediction, #RoadSafety"
     And the status is "ReadyToPublish"
-    When I publish the post on <social networks>
-    Then my post has a social networks status "published" 
+    When I publish the post on <social_network>
+    Then my post has a <social_network> status "published"
+
   Examples:
-    | social networks |
-    | LinkedIn        |
-    | Facebook        |
-    | SimpleBlog      |
+    | social_network |
+    | LinkedIn       |
+    | Facebook       |
+    | simpleBlog     |
 
   # Scenario Outline: Manage Content draft
   #   Given I have written a draft post for the page "Accident Predict":
--- tests/Features/FeatureContext.php
+++ tests/Features/FeatureContext.php
@@ -2,70 +2,133 @@
 
 namespace Features;
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use DoctrineTestingTools\DoctrineRepositoryTesterTrait;
+use SocialNetworksPublisher\Infrastructure\Shared\Symfony\Kernel;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+use function Safe\json_encode;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends WebTestCase implements Context
 {
+    private string $socialNetworksLinkedIn;
+    private string $socialNetworksSimpleBlog;
+    private string $socialNetworksFacebook;
+    private string $authorIdLinkedIn;
+    private string $authorIdFacebook;
+    private string $authorIdSimpleBlog;
+    private string $pageIdLinkedIn;
+    private string $pageIdFacebook;
+    private string $pageIdSimpleBlog;
+    private string $hashTags;
+    private string $status;
+    private string $postContent;
+    
+    private KernelBrowser $client;
+    
+    use DoctrineRepositoryTesterTrait;
+
+
+    public function __construct()
+    {
+        $this->initDoctrineTester();
+        $this->clearTables(["posts"]);
+
+        $this->client = static::createClient(["debug" => false]);
+    }
+
     /**
-     * @Given I am a LinkedIn author
+     * @Given I intend to publish on LinkedIn
      */
-    public function iAmALinkedinAuthor(): void
+    public function iIntendToPublishOnLinkedin()
+    {
+        $this->socialNetworksLinkedIn = "linkedIn";
+    }
+
+    /**
+     * @Given I have the LinkedIn author id :authorId
+     */
+    public function iHaveTheLinkedinAuthorId($authorId)
+    {
+        $this->authorIdLinkedIn = $authorId;
+    }
+
+    /**
+     * @Given I have the LinkedIn page id :pageId
+     */
+    public function iHaveTheLinkedinPageId($pageId)
     {
         throw new PendingException();
     }
 
     /**
-     * @Given I have written a post for the page :arg1:
+     * @Given I have written a post:
      */
-    public function iHaveWrittenAPostForThePage(string $arg1, PyStringNode $string): void
+    public function iHaveWrittenAPost(PyStringNode $postContent)
     {
-        throw new PendingException();
+        $this->postContent = $postContent;
     }
 
     /**
-     * @Given the hashtags are :arg1
+     * @Given the hashtags are :hashTags
      */
-    public function theHashtagsAre(string $arg1): void
+    public function theHashtagsAre($hashTags)
     {
-        throw new PendingException();
-    }
-
-
-
-    /**
-     * @Then my post has a scoial networks status :arg1
-     */
-    public function myPostHasAScoialNetworksStatus(string $arg1): void
-    {
-        throw new PendingException();
+        $this->hashTags = $hashTags;
     }
 
     /**
-     * @Given I am a Facebook author
+     * @Given the status is :status
      */
-    public function iAmAFacebookAuthor(): void
+    public function theStatusIs($status)
     {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given the status is :arg1
-     */
-    public function theStatusIs(string $arg1): void
-    {
-        throw new PendingException();
+        $this->status = $status;
     }
 
     /**
      * @When I publish the post on LinkedIn
      */
-    public function iPublishThePostOnLinkedin(): void
+    public function iPublishThePostOnLinkedin()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then my post has a LinkedIn status :arg1
+     */
+    public function myPostHasALinkedinStatus($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I intend to publish on Facebook
+     */
+    public function iIntendToPublishOnFacebook()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have the Facebook author id :authorId
+     */
+    public function iHaveTheFacebookAuthorId($authorId)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have the Facebook page id :pageId
+     */
+    public function iHaveTheFacebookPageId($pageId)
     {
         throw new PendingException();
     }
@@ -73,24 +136,70 @@ class FeatureContext implements Context
     /**
      * @When I publish the post on Facebook
      */
-    public function iPublishThePostOnFacebook(): void
+    public function iPublishThePostOnFacebook()
     {
         throw new PendingException();
     }
 
-     /**
-     * @Given I am a SimpleBlog author
+    /**
+     * @Then my post has a Facebook status :arg1
      */
-    public function iAmASimpleblogAuthor(): void
+    public function myPostHasAFacebookStatus($arg1)
     {
-        throw new PendingException(); 
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I intend to publish on SimpleBlog
+     */
+    public function iIntendToPublishOnSimpleblog()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have the SimpleBlog author id :authorId
+     */
+    public function iHaveTheSimpleblogAuthorId($authorId)
+    {
+        $this->authorIdSimpleBlog = $authorId;
+    }
+
+    /**
+     * @Given I have the SimpleBlog page id :pageId
+     */
+    public function iHaveTheSimpleblogPageId($pageId)
+    {
+        $this->pageIdSimpleBlog = $pageId;
     }
 
     /**
      * @When I publish the post on SimpleBlog
      */
-    public function iPublishThePostOnSimpleblog(): void
+    public function iPublishThePostOnSimpleblog()
     {
-        throw new PendingException(); 
+        $this->client->request(
+            "POST",
+            "api/v1/post/publish",
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                "socialNetworks" => $this->socialNetworksSimpleBlog,
+                "authorId" => $this->authorIdSimpleBlog,
+                "pageId" => $this->pageIdSimpleBlog,
+                "content" => $this->postContent,
+                "hashTags" => $this->hashTags,
+            ]),
+        );
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * @Then my post has a SimpleBlog status :arg1
+     */
+    public function myPostHasASimpleblogStatus($arg1)
+    {
+        throw new PendingException();
     }
 }
