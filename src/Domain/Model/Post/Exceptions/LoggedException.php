f676fb118027ddbb56c84f7e9bd9f20759dad41a
Pedro <pedrobesseofi@gmail.com>
2024-06-07 09:00:42 +0200

    Create the log file when he didnt exist

--- src/Domain/Model/Post/Exceptions/LoggedException.php
+++ src/Domain/Model/Post/Exceptions/LoggedException.php
@@ -6,15 +6,41 @@ use Safe\DateTime;
 use SocialNetworksPublisher\Infrastructure\Shared\CurrentWorkDirPath;
 
 use function Safe\error_log;
+use function Safe\fclose;
+use function Safe\file_put_contents;
+use function Safe\link;
+use function Safe\mkdir;
 
 class LoggedException extends \Exception
 {
     public const LOG_FILE_PATH = "/log/exceptions.log";
     private const LOG_PATTERN = "[%s] %d: %s\n";
+
     public function __construct(string $message, int $code)
     {
         parent::__construct($message, $code);
-        error_log($this->getMessageInFormat($message, $code), 3, CurrentWorkDirPath::getPath() . self::LOG_FILE_PATH);
+
+        $logFilePath = CurrentWorkDirPath::getPath() . self::LOG_FILE_PATH;
+        $this->ensureLogDirectoryExists(dirname($logFilePath));
+        $this->ensureLogFileExists($logFilePath);
+        error_log($this->getMessageInFormat($message, $code), 3, $logFilePath);
+    }
+
+    private function ensureLogDirectoryExists(string $directoryPath): void
+    {
+        if (!is_dir($directoryPath)) {
+            mkdir($directoryPath, 0755, true);
+        }
+    }
+
+    private function ensureLogFileExists(string $filePath): void
+    {
+        if (!file_exists($filePath)) {
+            $fileHandle = fopen($filePath, 'c+b');
+            if ($fileHandle !== false) {
+                fclose($fileHandle);
+            }
+        }
     }
 
     private function getMessageInFormat(string $message, int $code): string
--- tests/unit/Domain/Exceptions/LoggedExceptionTest.php
+++ tests/unit/Domain/Exceptions/LoggedExceptionTest.php
@@ -3,25 +3,50 @@
 namespace SocialNetworksPublisher\Tests\Domain\Model\Post\Exceptions;
 
 use PHPUnit\Framework\TestCase;
+use SocialNetworksPublisher\Domain\Model\Post\Author;
 use SocialNetworksPublisher\Domain\Model\Post\Exceptions\BadAuthorIdException;
 use SocialNetworksPublisher\Domain\Model\Post\Exceptions\LoggedException;
 use SocialNetworksPublisher\Infrastructure\Shared\CurrentWorkDirPath;
 
 use function Safe\file_get_contents;
+use function Safe\unlink;
 
 class LoggedExceptionTest extends TestCase
 {
+    private string $logFilePath;
+    protected function setUp(): void
+    {
+        $this->logFilePath = CurrentWorkDirPath::getPath() . LoggedException::LOG_FILE_PATH;
+    }
+
     public function testLoggedException(): void
     {
-        $filePath = CurrentWorkDirPath::getPath() . LoggedException::LOG_FILE_PATH;
-        if (file_exists($filePath)) {
-            unlink($filePath);
-        }
-        try {
-            throw new BadAuthorIdException("Log test", 0);
-        } catch (LoggedException $e) {
-            $logs = file_get_contents($filePath);
-            $this->assertStringEndsWith("0: Log test\n", $logs);
+        $this->deleteLogFile();
+        new BadAuthorIdException("Log test", 0);
+
+        $logs = file_get_contents($this->logFilePath);
+        $this->assertStringEndsWith("0: Log test\n", $logs);
+    }
+
+    public function testCheckCanCreateLogFile(): void
+    {
+        $this->deleteLogFile();
+
+        $sut = new LoggedException("a logged exception", 1);
+
+        $this->assertFileExists($this->logFilePath);
+    }
+
+    public function isExceptionThrowed(): void
+    {
+        $this->expectException(LoggedException::class);
+        new Author("df", "");
+    }
+
+    private function deleteLogFile(): void
+    {
+        if (file_exists($this->logFilePath)) {
+            unlink($this->logFilePath);
         }
     }
 }
