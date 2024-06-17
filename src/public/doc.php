8912892e575f04a1b9c7f4f74825414ea1b9292a
Pedro <pedrobesseofi@gmail.com>
2024-06-17 15:58:29 +0200

    Doesnt need public/

--- src/public/doc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-// Output the contents of the YAML file
-echo file_get_contents(__DIR__ . "/doc.yaml");
