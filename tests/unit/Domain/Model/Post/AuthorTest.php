3b7df6e4e9a94ff8d1669e47976c4702dbe3f446
Pedro <pedrobesseofi@gmail.com>
2024-06-19 11:25:26 +0200

    Feat: Edit to support the Enum SocialNetworks

--- tests/unit/Domain/Model/Post/AuthorTest.php
+++ tests/unit/Domain/Model/Post/AuthorTest.php
@@ -5,34 +5,23 @@ namespace SocialNetworksPublisher\Tests\Domain\Model\Post;
 use PHPUnit\Framework\TestCase;
 use SocialNetworksPublisher\Domain\Model\Post\Author;
 use SocialNetworksPublisher\Domain\Model\Post\Exceptions\BadAuthorIdException;
-use SocialNetworksPublisher\Domain\Model\Post\Exceptions\BadSocialNetworksParameterException;
 
 class AuthorTest extends TestCase
 {
     public function testOrganizationAuthor(): void
     {
-        $author = new Author('FaceBooK', '248178251');
-        $author2 = new Author('LinkedIn', '248178251');
+        $author = new Author('248178251');
+        $author2 = new Author('248f17825a1');
 
         $this->assertInstanceOf(Author::class, $author);
         $this->assertEquals('248178251', $author->getId());
-        $this->assertEquals('facebook', $author->getSocialNetwork());
-        $this->assertEquals('linkedin', $author2->getSocialNetwork());
+        $this->assertEquals('248f17825a1', $author2->getId());
     }
-
-    public function testBadAuthorUrl(): void
-    {
-        $this->expectException(BadSocialNetworksParameterException::class);
-        $this->expectExceptionMessage("The social network parameters cannot be empty");
-        $this->expectExceptionCode(BadSocialNetworksParameterException::ERROR_CODE);
-        new Author("", "123156");
-    }
-
     public function testBadAuthorId(): void
     {
         $this->expectException(BadAuthorIdException::class);
         $this->expectExceptionMessage("The id parameters cannot be empty");
         $this->expectExceptionCode(BadAuthorIdException::ERROR_CODE);
-        new Author("facebook", "");
+        new Author("");
     }
 }
--- tests/unit/Domain/Model/Post/PageTest.php
+++ tests/unit/Domain/Model/Post/PageTest.php
@@ -11,21 +11,10 @@ class PageTest extends TestCase
 {
     public function testValidPage(): void
     {
-        $page = new Page("faceBook", "123456");
-        $page2 = new Page("LinkedIn", "456");
-
-        $this->assertEquals("facebook", $page->getSocialNetwork());
+        $page = new Page("123456");
+        $page2 = new Page("456");
         $this->assertEquals("123456", $page->getId());
-        $this->assertEquals("linkedin", $page2->getSocialNetwork());
         $this->assertEquals("456", $page2->getId());
-    }
-
-    public function testBadPageSocialNetworksException(): void
-    {
-        $this->expectException(BadSocialNetworksParameterException::class);
-        $this->expectExceptionCode(BadSocialNetworksParameterException::ERROR_CODE);
-        $this->expectExceptionMessage("The social network parameters cannot be empty");
-        new Page("", "456");
     }
 
     public function testBadPageId(): void
@@ -33,6 +22,6 @@ class PageTest extends TestCase
         $this->expectException(BadPageIdException::class);
         $this->expectExceptionCode(BadPageIdException::ERROR_CODE);
         $this->expectExceptionMessage("The id parameters cannot be empty");
-        new Page("facebook", "");
+        new Page("");
     }
 }
