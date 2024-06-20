d010e2560856affd5365bc807f3c87c23550bf9c
Pedro <pedrobesseofi@gmail.com>
2024-06-20 11:33:01 +0200

    Test: Twitter in factory

--- tests/unit/Infrastructure/Provider/FactorySocialNetworksApiTest.php
+++ tests/unit/Infrastructure/Provider/FactorySocialNetworksApiTest.php
@@ -7,8 +7,11 @@ use SocialNetworksPublisher\Application\Service\PublishPost\AbstractFactorySocia
 use SocialNetworksPublisher\Application\Service\PublishPost\SocialNetworksApiInterface;
 use SocialNetworksPublisher\Domain\Model\Post\SocialNetworks;
 use SocialNetworksPublisher\Infrastructure\Provider\Exceptions\InvalidSocialNetworks;
+use SocialNetworksPublisher\Infrastructure\Provider\Facebook\Facebook;
 use SocialNetworksPublisher\Infrastructure\Provider\FactorySocialNetworksApi;
+use SocialNetworksPublisher\Infrastructure\Provider\LinkedIn\LinkedIn;
 use SocialNetworksPublisher\Infrastructure\Provider\SimpleBlog\SimpleBlog;
+use SocialNetworksPublisher\Infrastructure\Provider\Twitter\Twitter;
 
 class FactorySocialNetworksApiTest extends TestCase
 {
@@ -23,13 +26,20 @@ class FactorySocialNetworksApiTest extends TestCase
     {
         $socialNetworks = SocialNetworks::Facebook;
         $sut = (new FactorySocialNetworksApi())->buildApi($socialNetworks);
-        $this->assertInstanceOf(SocialNetworksApiInterface::class, $sut);
+        $this->assertInstanceOf(Facebook::class, $sut);
     }
 
     public function testAbstractFactoryLinkedIn(): void
     {
         $socialNetworks = SocialNetworks::LinkedIn;
         $sut = (new FactorySocialNetworksApi())->buildApi($socialNetworks);
-        $this->assertInstanceOf(SocialNetworksApiInterface::class, $sut);
+        $this->assertInstanceOf(LinkedIn::class, $sut);
+    }
+
+    public function testAbstractFactoryTwitter(): void
+    {
+        $socialNetworks = SocialNetworks::Twitter;
+        $sut = (new FactorySocialNetworksApi())->buildApi($socialNetworks);
+        $this->assertInstanceOf(Twitter::class, $sut);
     }
 }
