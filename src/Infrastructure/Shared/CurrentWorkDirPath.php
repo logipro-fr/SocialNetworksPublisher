72c263454abdb1b3676c69188419eff307e82d08
Pedro <pedrobesseofi@gmail.com>
2024-06-28 09:29:46 +0200

    Refacto: No exceptions needed

--- src/Infrastructure/Shared/CurrentWorkDirPath.php
+++ src/Infrastructure/Shared/CurrentWorkDirPath.php
@@ -2,16 +2,10 @@
 
 namespace SocialNetworksPublisher\Infrastructure\Shared;
 
-use SocialNetworksPublisher\Infrastructure\Exceptions\NoPWDException;
-
 class CurrentWorkDirPath
 {
-    public static function getPath(string $dir = __DIR__): string
+    public static function getPath(): string
     {
-        if (!empty($dir)) {
-            return dirname(__DIR__, 3);
-        }
-
-        throw new NoPWDException("Environment variable PWD not found");
+        return dirname(__DIR__, 3);
     }
 }
--- tests/unit/Infrastructure/Shared/CurrentWorkDirPathTest.php
+++ tests/unit/Infrastructure/Shared/CurrentWorkDirPathTest.php
@@ -3,8 +3,6 @@
 namespace SocialNetworksPublisher\Tests\Infrastructure;
 
 use PHPUnit\Framework\TestCase;
-use ReflectionClass;
-use SocialNetworksPublisher\Infrastructure\Exceptions\NoPWDException;
 use SocialNetworksPublisher\Infrastructure\Shared\CurrentWorkDirPath;
 
 class CurrentWorkDirPathTest extends TestCase
@@ -14,14 +12,5 @@ class CurrentWorkDirPathTest extends TestCase
         $currentDir = dirname(__DIR__, 4);
         $path = CurrentWorkDirPath::getPath();
         $this->assertEquals($currentDir, $path);
-    }
-
-    public function testNoPWDException(): void
-    {
-
-        $this->expectException(NoPWDException::class);
-        $this->expectExceptionMessage("Environment variable PWD not found");
-
-        CurrentWorkDirPath::getPath('');
     }
 }
