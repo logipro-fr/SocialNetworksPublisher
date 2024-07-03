ab08e98352d10b223f78f658ad560667900ed915
Pedro <pedrobesseofi@gmail.com>
2024-07-03 15:24:56 +0200

    refacto

--- tests/unit/Infrastructure/Provider/Twitter/TwitterClientTest.php
+++ tests/unit/Infrastructure/Provider/Twitter/TwitterClientTest.php
@@ -2,6 +2,7 @@
 
 namespace SocialNetworksPublisher\Tests\Infrastructure\Provider\Twitter;
 
+use DateTime;
 use PHPUnit\Framework\TestCase;
 use SocialNetworksPublisher\Application\Service\PublishPost\PostFactory;
 use SocialNetworksPublisher\Application\Service\PublishPost\PublishPostRequest;
@@ -17,11 +18,15 @@ use function Safe\file_get_contents;
 
 class TwitterClientTest extends TestCase
 {
+    private const BEARER_PATH = "/var/test_TwitterBearerToken.txt";
+    private const REFRESH_PATH = "/var/test_TwitterRefreshToken.txt";
+    private const EXPIRATION_PATH = "/var/test_TwitterTokenExpiration.txt";
     private const TEXT_CONTENT = "Hello world !";
     private PublishPostRequest $request;
 
     protected function setUp(): void
     {
+        $this->cleanup();
         $this->request = new PublishPostRequest(
             "Twitter",
             "1a84fvb",
@@ -45,13 +50,16 @@ class TwitterClientTest extends TestCase
 
     public function testTwitterRequest(): void
     {
-        $this->cleanup();
-
         $post = (new PostFactory())->buildPostFromRequest($this->request);
-        $twitterResponse1 = [
+        $tokenBearer = [
             new MockResponse(
                 file_get_contents(CurrentWorkDirPath::getPath() .
                  '/tests/unit/ressources/TwitterResponseRefreshFirst.json'),
+                ['http_code' => 200]
+            ),
+            new MockResponse(
+                file_get_contents(CurrentWorkDirPath::getPath() .
+                 '/tests/unit/ressources/TwitterResponseRefresh.json'),
                 ['http_code' => 200]
             ),
         ];
@@ -61,79 +69,9 @@ class TwitterClientTest extends TestCase
                  '/tests/unit/ressources/TwitterResponseTweet.json'),
                 ['http_code' => 200]
             ),
-        ];
-        $twitterResponse3 = [
-            new MockResponse(
-                file_get_contents(CurrentWorkDirPath::getPath() .
-                 '/tests/unit/ressources/TwitterResponseRefresh.json'),
-                ['http_code' => 200]
-            ),
             new MockResponse(
                 file_get_contents(CurrentWorkDirPath::getPath() .
                  '/tests/unit/ressources/TwitterResponseTweet.json'),
-                ['http_code' => 200]
-            ),
-        ];
-        $client1 = new MockHttpClient($twitterResponse1, 'https://api.twitter.com/2/oauth2/token');
-        $client2 = new MockHttpClient($twitterResponse2, 'https://api.twitter.com/2/tweets');
-        $client3 = new MockHttpClient($twitterResponse3, 'https://api.twitter.com/2/oauth2/token');
-
-        $bearerToken = new TwitterBearerToken();
-
-        $twitter = new TwitterClient($client1, $bearerToken);
-        $refreshToken1 = $bearerToken->getRefreshToken();
-        $accesToken1 = $bearerToken->getBearerToken();
-
-        $twitter2 = new TwitterClient($client2, $bearerToken);
-        $response2 = $twitter2->postApiRequest($post);
-
-        $expirationDate = new \DateTime('-2 hour');
-        $bearerToken->setBearerToken('testBearerToken', $expirationDate);
-        $twitter3 = new TwitterClient($client3, $bearerToken);
-        $response3 = $twitter3->postApiRequest($post);
-        $refreshToken3 = $bearerToken->getRefreshToken();
-        $accesToken3 = $bearerToken->getBearerToken();
-
-
-
-        $this->assertEquals('refresh_token', $refreshToken1);
-        $this->assertEquals('access_token', $accesToken1);
-
-        $this->assertTrue($response2->success);
-
-        $this->assertEquals('refresh_token3', $refreshToken3);
-        $this->assertEquals('access_token3', $accesToken3);
-        $this->assertTrue($response3->success);
-    }
-
-    public function testBadRequestException(): void
-    {
-        $this->expectException(BadRequestException::class);
-        $this->expectExceptionCode(400);
-        $this->expectExceptionMessage("Invalid request");
-        $twitterResponse1 = [
-            new MockResponse(
-                file_get_contents(CurrentWorkDirPath::getPath() .
-                 '/tests/unit/ressources/TwitterBadRequestResponse.json'),
-                ['http_code' => 400]
-            ),
-        ];
-        $client1 = new MockHttpClient($twitterResponse1, 'https://api.twitter.com/2/oauth2/token');
-        $bearerToken = new TwitterBearerToken();
-        $twitter = new TwitterClient($client1, $bearerToken);
-    }
-
-    public function testUnauthorizedException(): void
-    {
-        $this->expectException(UnauthorizedException::class);
-        $this->expectExceptionMessage("Unauthorized");
-
-        $post = (new PostFactory())->buildPostFromRequest($this->request);
-
-        $twitterResponse1 = [
-            new MockResponse(
-                file_get_contents(CurrentWorkDirPath::getPath() .
-                 '/tests/unit/ressources/TwitterResponseRefreshFirst.json'),
                 ['http_code' => 200]
             ),
             new MockResponse(
@@ -142,9 +80,38 @@ class TwitterClientTest extends TestCase
                 ['http_code' => 401]
             ),
         ];
-        $client1 = new MockHttpClient($twitterResponse1, 'https://api.twitter.com/2/oauth2/token');
-        $bearerToken = new TwitterBearerToken();
-        $twitter = new TwitterClient($client1, $bearerToken);
-        $response = $twitter->postApiRequest($post);
+        $tokenBearerClient = new MockHttpClient($tokenBearer, 'https://api.twitter.com/2/oauth2/token');
+        $TwitterClient = new MockHttpClient($twitterResponse2, 'https://api.twitter.com/2/tweets');
+        $token = new TwitterBearerToken(
+            $tokenBearerClient,
+            self::BEARER_PATH,
+            self::REFRESH_PATH,
+            self::EXPIRATION_PATH
+        );
+
+        $sut = new TwitterClient($TwitterClient, $token);
+        $refreshToken1 = $token->getRefreshToken();
+        $accessToken1 = $token->getBearerToken();
+
+        $expirationDate = new \DateTime('-2 hour');
+        file_put_contents(
+            CurrentWorkDirPath::getPath() . self::EXPIRATION_PATH,
+            $expirationDate->format(DateTime::ATOM)
+        );
+        $postResponse = $sut->postApiRequest($post);
+
+        $postResponse2 = $sut->postApiRequest($post);
+        $refreshToken2 = $token->getRefreshToken();
+        $accessToken2 = $token->getBearerToken();
+
+        $this->assertTrue($postResponse->success);
+        $this->assertNotEquals($refreshToken1, $refreshToken2);
+        $this->assertNotEquals($accessToken1, $accessToken2);
+        $this->assertTrue($postResponse2->success);
+
+        $this->expectException(UnauthorizedException::class);
+        $this->expectExceptionMessage("Unauthorized");
+
+        $sut->postApiRequest($post);
     }
 }
