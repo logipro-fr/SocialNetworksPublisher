41a13a693771d7931ced56175c49caf5d931064e
Pedro <pedrobesseofi@gmail.com>
2024-08-05 11:03:39 +0200

    Use of HttpFoundation error code

--- src/Infrastructure/Provider/Exceptions/BadRequestException.php
+++ src/Infrastructure/Provider/Exceptions/BadRequestException.php
@@ -3,8 +3,9 @@
 namespace SocialNetworksPublisher\Infrastructure\Provider\Exceptions;
 
 use SocialNetworksPublisher\Domain\Model\Post\Exceptions\LoggedException;
+use Symfony\Component\HttpFoundation\Response;
 
 class BadRequestException extends LoggedException
 {
-    public const ERROR_CODE = 400;
+    public const ERROR_CODE = Response::HTTP_BAD_REQUEST;
 }
