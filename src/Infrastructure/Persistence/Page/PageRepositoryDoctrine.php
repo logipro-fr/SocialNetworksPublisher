11994ba6683e1b98f2635c5c42bf8503f166c05e
Pedro <pedrobesseofi@gmail.com>
2024-08-20 15:32:04 +0200

    Feat: Repository

---
+++ src/Infrastructure/Persistence/Page/PageRepositoryDoctrine.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SocialNetworksPublisher\Infrastructure\Persistence\Page;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use SocialNetworksPublisher\Domain\Model\Page\Exceptions\PageNotFoundException;
+use SocialNetworksPublisher\Domain\Model\Page\Page;
+use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+
+/**
+ * @extends EntityRepository<Page>
+ */
+class PageRepositoryDoctrine extends EntityRepository implements PageRepositoryInterface {
+    public function __construct(EntityManagerInterface $em)
+    {
+        $class = $em->getClassMetadata(Page::class);
+        parent::__construct($em, $class);
+    }
+    public function add(Page $page): void
+    {
+        
+        $this->getEntityManager()->persist($page);
+    }
+
+    public function findById(PageId $pageId): Page
+    {
+        $page = $this->getEntityManager()->find(Page::class, $pageId);
+        if ($page == null) {
+            throw new PageNotFoundException($pageId);
+        }
+        return $page;
+    }
+}
---
+++ src/Infrastructure/Persistence/Page/PageRepositoryInMemory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SocialNetworksPublisher\Infrastructure\Persistence\Page;
+
+use SocialNetworksPublisher\Domain\Model\Page\Exceptions\PageAlreadyExistsException;
+use SocialNetworksPublisher\Domain\Model\Page\Exceptions\PageNotFoundException;
+use SocialNetworksPublisher\Domain\Model\Page\Page;
+use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+
+class PageRepositoryInMemory implements PageRepositoryInterface{
+
+    /** @var array<string, Page> */
+    private array $pages = [];
+    public function add(Page $page): void
+    {
+        if (array_key_exists($id = $page->getPageId()->__toString(), $this->pages)) {
+            throw new PageAlreadyExistsException($id);
+        }
+        $this->pages[$page->getPageId()->__toString()] = $page;
+    }
+
+    public function findById(PageId $pageId): Page
+    {
+        if (!array_key_exists($id = $pageId->__toString(), $this->pages)) {
+            throw new PageNotFoundException($id);
+        }
+
+        return $this->pages[$id];
+    }
+}
