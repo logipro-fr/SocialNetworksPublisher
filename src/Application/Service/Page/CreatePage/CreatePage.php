92a79619f0d82f3ff52c2692d466ac9f02d22433
Pedro <pedrobesseofi@gmail.com>
2024-08-20 15:35:18 +0200

    Feat : Service create Page

---
+++ src/Application/Service/Page/CreatePage/CreatePage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SocialNetworksPublisher\Application\Service\Page\CreatePage;
+
+use SocialNetworksPublisher\Domain\Model\Page\Page;
+use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageName;
+use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+use SocialNetworksPublisher\Domain\Model\Shared\SocialNetworks;
+
+class CreatePage {
+    private CreatePageResponse $response;
+    public function __construct(private PageRepositoryInterface $pages)
+    {
+        
+    }
+
+    public function execute(CreatePageRequest $request) {
+        $this->pages->add(new Page(
+            $pageId = new PageId(),
+            new PageName($request->pageName),
+            SocialNetworks::fromString($request->socialNetworks)
+        ));
+
+        $this->response = new CreatePageResponse($pageId);
+    }
+
+    public function getResponse(): CreatePageResponse {
+        return $this->response;
+    }
+}
---
+++ src/Application/Service/Page/CreatePage/CreatePageResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SocialNetworksPublisher\Application\Service\Page\CreatePage;
+
+class CreatePageResponse{
+    public function __construct(readonly string $pageId)
+    {
+        
+    }
+}
