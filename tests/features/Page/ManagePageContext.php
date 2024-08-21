8521c1834d70468411d34b46f8ecdcf6d8b50ab6
Pedro <pedrobesseofi@gmail.com>
2024-08-21 15:02:01 +0200

    Behat test

---
+++ tests/features/Page/ManagePageContext.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace features\Page;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\MockObject\Generator\Generator;
+use PHPUnit\Framework\MockObject\MockObject;
+use SocialNetworksPublisher\Domain\Model\Page\Page;
+use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageName;
+use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+use SocialNetworksPublisher\Domain\Model\Shared\SocialNetworks;
+use SocialNetworksPublisher\Infrastructure\Api\V1\CreatePageController;
+use SocialNetworksPublisher\Infrastructure\Persistence\Page\PageRepositoryInMemory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Defines application features from the specific context.
+ */
+class ManagePageContext implements Context
+{
+
+    private PageRepositoryInterface $pages;
+    private CreatePageController $createPageController;
+    private string $socialNetwork;
+    private string $pageName;
+    private Response $createPageResponse;
+    public function __construct()
+    {
+        $this->pages = new PageRepositoryInMemory();
+
+        /** @var MockObject $entityManager */
+        $entityManager = (new Generator())->testDouble(
+            EntityManagerInterface::class,
+            true,
+            true,
+            callOriginalConstructor: false
+        );
+        /** @var EntityManagerInterface $entityManager */
+        $this->createPageController = new CreatePageController(
+            $this->pages,
+            $entityManager
+        );
+
+    }
+
+    /**
+     * @Given I want to create a page on :socialNetwork
+     */
+    public function iWantToCreateAPageOn(string $socialNetwork)
+    {
+        $this->socialNetwork = $socialNetwork;
+    }
+
+    /**
+     * @Given I choose the page name :pageName
+     */
+    public function iChooseThePageName(string $pageName)
+    {
+        $this->pageName = $pageName;
+    }
+
+    /**
+     * @When I create this Page
+     */
+    public function iCreateThisPage()
+    {
+        $request = Request::create(
+            "/api/v1/pages",
+            "POST",
+            content: json_encode([
+                "pageName" => $this->pageName,
+                "socialNetwork" => $this->socialNetwork
+            ])
+        );
+        $this->createPageResponse = $this->createPageController->execute($request);
+    }
+
+    /**
+     * @Then The page is created and I have the pageId
+     */
+    public function thePageIsCreatedAndIHaveThePageId()
+    {
+        /** @var string $content */
+        $content = $this->createPageResponse->getContent();
+        /** @var \stdClass $response */
+        $response = json_decode($content);
+
+        $page = $this->pages->findById(new PageId($response->data->pageId));
+        Assert::assertNotNull($page, "The page was not created.");
+        Assert::assertEquals("Page name", $page->getName());
+        Assert::assertEquals(SocialNetworks::tryFrom($this->socialNetwork), $page->getSocialNetwork());
+    }
+}
