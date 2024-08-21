8b1537022c90275044177596d5875b5a9daa758e
Pedro <pedrobesseofi@gmail.com>
2024-08-21 11:23:09 +0200

    Controller

--- tests/unit/Infrastructure/Api/V1/AddPostControllerTest.php
+++ tests/unit/Infrastructure/Api/V1/AddPostControllerTest.php
@@ -5,73 +5,75 @@ namespace SocialNetworksPublisher\Tests\Infrastructure\Api\V1;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use SocialNetworksPublisher\Domain\Model\Page\Exceptions\PageNotFoundException;
+use SocialNetworksPublisher\Domain\Model\Page\Page;
 use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageName;
 use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
-use SocialNetworksPublisher\Domain\Model\Post\Exceptions\BadSocialNetworksParameterException;
 use SocialNetworksPublisher\Domain\Model\Shared\SocialNetworks;
-use SocialNetworksPublisher\Infrastructure\Api\V1\CreatePageController;
+use SocialNetworksPublisher\Infrastructure\Api\V1\AddPostController;
 use SocialNetworksPublisher\Infrastructure\Persistence\Page\PageRepositoryInMemory;
 use Symfony\Component\HttpFoundation\Request;
 
 use function Safe\json_encode;
 
-class CreatePageControllerTest extends TestCase {
+class AddPostControllerTest extends TestCase {
     use AssertResponseTrait;
 
     private PageRepositoryInterface $pages;
-    private CreatePageController $controller;
+    private AddPostController $controller;
     private MockObject $emMock;
-
     public function setUp(): void{
         $this->pages = new PageRepositoryInMemory;
-
+    
 
          /** @var MockObject $entityManager */
          $entityManager = $this->createMock(EntityManagerInterface::class);
          $this->emMock = $entityManager;
 
-         $this->controller = new CreatePageController(
+         $this->controller = new AddPostController(
             $this->pages,
             $entityManager,
         );
- 
     }
 
     public function testAddPost(): void {
-        $this->emMock->expects($this->once())->method("flush");
+
+
+        $originalPage = new Page(
+            $id= new PageId(),
+            new PageName(""),
+            SocialNetworks::Twitter,
+        );
+
+        $this->pages->add($originalPage);
+
+
         $request = Request::create(
-            "/api/v1/pages",
+            "/api/v1/pages/post",
             "POST",
             content: json_encode([
-                "pageName" => "page_name",
-                "socialNetwork" => "Twitter"
+                "pageId" => $id->__toString(),
+                "content" => "test_content"
             ])
         );
 
         $response = $this->controller->execute($request);
-
-        /** @var string $content */
-        $content = $response->getContent();
-        /** @var \stdClass $responseObject */
-        $responseObject = json_decode($content);
-
         $this->assertEquals(201, $response->getStatusCode());
-        $this->assertResponseSuccess($response, (object)["pageId" => $responseObject->data->pageId]);
-
-        $createdAccount = $this->pages
-        ->findById(new PageId($responseObject->data->pageId));
-        $this->assertEquals("page_name", $createdAccount->getName());
-        $this->assertEquals(SocialNetworks::Twitter, $createdAccount->getSocialNetwork());
+        $modifiedPage = $this->pages
+        ->findById(new PageId($id));
+        $this->assertCount(1, $modifiedPage->getPosts());
+        $this->assertEquals("test_content", $modifiedPage->getPosts()[0]->getContent() );
     }
 
     public function testExceptionRaised(): void {
 
         $request = Request::create(
-            "/api/v1/pages",
+            "/api/v1/pages/post",
             "POST",
             content: json_encode([
-                "pageName" => "page_name",
-                "socialNetwork" => ""
+                "pageId" => "test",
+                "content" => "test_content"
             ])
         );
 
@@ -79,7 +81,7 @@ class CreatePageControllerTest extends TestCase {
 
         $this->assertResponseFailure(
             $response,
-            (new \ReflectionClass(BadSocialNetworksParameterException::class))->getShortName()
+            (new \ReflectionClass(PageNotFoundException::class))->getShortName()
         );
     }
 }
