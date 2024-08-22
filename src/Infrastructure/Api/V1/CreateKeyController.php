4324cc8faf6bfbfb196328d9370dda34b18cd03d
Pedro <pedrobesseofi@gmail.com>
2024-08-22 16:21:46 +0200

    CreateKey controller

---
+++ src/Infrastructure/Api/V1/CreateKeyController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SocialNetworksPublisher\Infrastructure\Api\V1;
+
+use Doctrine\ORM\EntityManagerInterface;
+use SocialNetworksPublisher\Application\Service\Key\CreateKey\CreateKey;
+use SocialNetworksPublisher\Application\Service\Key\CreateKey\CreateKeyTwitterRequest;
+use SocialNetworksPublisher\Domain\Model\Key\KeyRepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateKeyController extends AbstractController
+{
+    public function __construct(
+        private KeyRepositoryInterface $keys,
+        private EntityManagerInterface $em,
+    ) {
+    }
+    #[Route("api/v1/keys/twitter", name:"keys_twitter", methods:["POST"])]
+    public function execute(Request $request): Response
+    {
+        try {
+            $createKeyRequest = $this->convertToTwitterCreateKeyRequest($request);
+            $service = new CreateKey($this->keys);
+            $service->execute($createKeyRequest);
+            $this->em->flush();
+            return $this->writeSuccessfulResponse($service->getResponse(), Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return $this->writeUnsuccessfulResponse($e);
+        }
+    }
+
+    public function convertToTwitterCreateKeyRequest(Request $request): CreateKeyTwitterRequest
+    {
+        /** @var string $content */
+        $content = $request->getContent();
+        /** @var \stdClass $requestObject */
+        $requestObject = json_decode($content);
+        return new CreateKeyTwitterRequest(
+            $requestObject->bearerToken,
+            $requestObject->refreshToken,
+        );
+    }
+}
---
+++ tests/unit/Infrastructure/Api/V1/CreateKeyControllerTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace SocialNetworksPublisher\Tests\Infrastructure\Api\V1;
+
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use SocialNetworksPublisher\Domain\Model\Key\Exceptions\KeyDataEmptyBearerTokenException;
+use SocialNetworksPublisher\Domain\Model\Key\KeyId;
+use SocialNetworksPublisher\Domain\Model\Key\KeyRepositoryInterface;
+use SocialNetworksPublisher\Domain\Model\Key\TwitterKeyData;
+use SocialNetworksPublisher\Domain\Model\Shared\Exceptions\SocialNetworksDoesntExist;
+use SocialNetworksPublisher\Domain\Model\Page\PageId;
+use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+use SocialNetworksPublisher\Domain\Model\Shared\SocialNetworks;
+use SocialNetworksPublisher\Infrastructure\Api\V1\CreateKeyController;
+use SocialNetworksPublisher\Infrastructure\Api\V1\CreatePageController;
+use SocialNetworksPublisher\Infrastructure\Persistence\Key\KeyRepositoryInMemory;
+use SocialNetworksPublisher\Infrastructure\Persistence\Page\PageRepositoryInMemory;
+use Symfony\Component\HttpFoundation\Request;
+
+use function Safe\json_encode;
+
+class CreateKeyControllerTest extends TestCase
+{
+    use AssertResponseTrait;
+
+    private KeyRepositoryInMemory $keys;
+    private CreateKeyController $controller;
+    private MockObject $emMock;
+    public function setUp(): void
+    {
+        $this->keys = new KeyRepositoryInMemory();
+         /** @var MockObject $entityManager */
+         $entityManager = $this->createMock(EntityManagerInterface::class);
+         $this->emMock = $entityManager;
+        /** @var EntityManagerInterface $entityManager */
+         $this->controller = new CreateKeyController(
+             $this->keys,
+             $entityManager,
+         );
+    }
+
+    public function testCreatePage(): void
+    {
+        $this->emMock->expects($this->once())->method("flush");
+        $request = Request::create(
+            "/api/v1/keys/twitter",
+            "POST",
+            content: json_encode([
+                "bearerToken" => "bearer",
+                "refreshToken" => "refresh"
+            ])
+        );
+
+        $response = $this->controller->execute($request);
+
+        /** @var string $content */
+        $content = $response->getContent();
+        /** @var \stdClass $responseObject */
+        $responseObject = json_decode($content);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertResponseSuccess($response, (object)["keyId" => $responseObject->data->keyId]);
+
+        $createdKey = $this->keys
+        ->findById(new KeyId($responseObject->data->keyId));
+        $this->assertEquals(SocialNetworks::Twitter, $createdKey->getSocialNetwork());
+        /** @var TwitterKeyData */
+        $keyData = $createdKey->getKeyData();
+
+        $this->assertEquals("bearer", $keyData->getBearerToken());
+        $this->assertEquals("refresh", $keyData->getRefreshToken());
+    }
+
+    public function testExceptionRaised(): void
+    {
+
+        $request = Request::create(
+            "/api/v1/pages",
+            "POST",
+            content: json_encode([
+                "bearerToken" => "",
+                "refreshToken" => ""
+            ])
+        );
+
+        $response = $this->controller->execute($request);
+
+        $this->assertResponseFailure(
+            $response,
+            (new \ReflectionClass(KeyDataEmptyBearerTokenException::class))->getShortName()
+        );
+    }
+}
