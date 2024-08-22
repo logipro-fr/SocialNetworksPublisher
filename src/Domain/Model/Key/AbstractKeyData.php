0aa11467f1c09e439649a38825a3288ce1ff29bc
Pedro <pedrobesseofi@gmail.com>
2024-08-22 11:46:41 +0200

    Feat : KeyData object with twitter and Linked integration

---
+++ src/Domain/Model/Key/AbstractKeyData.php
@@ -0,0 +1,14 @@
+<?php
+namespace SocialNetworksPublisher\Domain\Model\Key;
+
+abstract class AbstractKeyData {
+    protected string $bearerToken;
+    abstract function checkBearerToken(): void;
+    public function getBearerToken(): string {
+        return $this->bearerToken;
+    }
+
+    public function setBearerToken(string $bearerToken): void {
+        $this->bearerToken = $bearerToken;
+    }
+}
---
+++ src/Domain/Model/Key/Exceptions/KeyDataEmptyBearerTokenException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SocialNetworksPublisher\Domain\Model\Key\Exceptions;
+
+use SocialNetworksPublisher\Domain\Model\Shared\Exceptions\LoggedException;
+
+class KeyDataEmptyBearerTokenException extends LoggedException {
+    public const MESSAGE = "Empty key bearer token";
+    public const ERROR_CODE = 404;
+
+    public function __construct(
+    ) {
+        parent::__construct(self::MESSAGE, self::ERROR_CODE);
+    }
+}
