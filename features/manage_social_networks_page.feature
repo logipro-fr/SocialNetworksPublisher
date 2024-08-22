a1e726180a64e6fbcdb4f042c865500dfc442da2
Pedro <pedrobesseofi@gmail.com>
2024-08-22 08:56:06 +0200

    Behat test

--- features/manage_social_networks_page.feature
+++ features/manage_social_networks_page.feature
@@ -14,3 +14,14 @@ Feature: Manage Social Networks Page
       | Twitter        |
       | SimpleBlog     |
       | LinkedIn       |
+
+  Scenario Outline: Add post on Page
+    Given I have a page with the pageId "<pageId>"
+    And I want to create a post with the content "<postContent>"
+    When I add this post
+    Then the post is added to the page with the post status "ready"
+
+    Examples:
+      | pageId  | postContent   |
+      | page_1  | First post    |
+      | page_2  | Second post   |
--- tests/features/Page/ManagePageContext.php
+++ tests/features/Page/ManagePageContext.php
@@ -13,7 +13,9 @@ use SocialNetworksPublisher\Domain\Model\Page\Page;
 use SocialNetworksPublisher\Domain\Model\Page\PageId;
 use SocialNetworksPublisher\Domain\Model\Page\PageName;
 use SocialNetworksPublisher\Domain\Model\Page\PageRepositoryInterface;
+use SocialNetworksPublisher\Domain\Model\Page\PostStatus;
 use SocialNetworksPublisher\Domain\Model\Shared\SocialNetworks;
+use SocialNetworksPublisher\Infrastructure\Api\V1\AddPostController;
 use SocialNetworksPublisher\Infrastructure\Api\V1\CreatePageController;
 use SocialNetworksPublisher\Infrastructure\Persistence\Page\PageRepositoryInMemory;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,11 +23,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ManagePageContext implements Context
 {
+    /** PART 1 */
     private PageRepositoryInterface $pages;
     private CreatePageController $createPageController;
     private string $socialNetwork;
     private string $pageName;
     private Response $createPageResponse;
+
+    /** PART 2 */
+    private string $pageId;
+    private string $content;
+    private AddPostController $addPostController;
+    private Response $addPostResponse;
     public function __construct()
     {
         $this->pages = new PageRepositoryInMemory();
@@ -41,6 +50,11 @@ class ManagePageContext implements Context
         $this->createPageController = new CreatePageController(
             $this->pages,
             $entityManager
+        );
+
+        $this->addPostController = new AddPostController(
+            $this->pages,
+            $entityManager,
         );
     }
 
@@ -90,5 +104,56 @@ class ManagePageContext implements Context
         Assert::assertNotNull($page, "The page was not created.");
         Assert::assertEquals("Page name", $page->getName());
         Assert::assertEquals(SocialNetworks::tryFrom($this->socialNetwork), $page->getSocialNetwork());
+    }
+
+    /**
+     * @Given I have a page with the pageId :pageId
+     */
+    public function iHaveAPageWithThePageid(string $pageId): void
+    {
+        $this->pageId = $pageId;
+        $this->pages->add(new Page(
+            new PageId($pageId),
+            new PageName(""),
+            SocialNetworks::Twitter,
+        ));
+    }
+
+    /**
+     * @Given I want to create a post with the content :content
+     */
+    public function iWantToCreateAPostWithTheContent(string $content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @When I add this post
+     */
+    public function iAddThisPost()
+    {
+        $request = Request::create(
+            "/api/v1/pages/post",
+            "POST",
+            content: json_encode([
+                "pageId" => $this->pageId,
+                "content" => $this->content
+            ])
+        );
+        $this->addPostResponse = $this->addPostController->execute($request);
+    }
+
+    /**
+     * @Then the post is added to the page with the post status :status
+     */
+    public function thePostIsAddedToThePageWithThePostStatus($status)
+    {
+        /** @var string $content */
+        $content = $this->addPostResponse->getContent();
+        /** @var \stdClass $response */
+        $response = json_decode($content);
+        $page = $this->pages->findById(new PageId($this->pageId));
+        Assert::assertCount(1, $page->getPosts());
+        Assert::assertEquals(PostStatus::tryFrom($status), $page->getPosts()[0]->getStatus());
     }
 }
