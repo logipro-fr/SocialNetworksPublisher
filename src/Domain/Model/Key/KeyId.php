b537f29937b634c2a0e353bc5627fc5f8a648318
Pedro <pedrobesseofi@gmail.com>
2024-08-22 11:54:03 +0200

    Feat : KeyId for Key

---
+++ src/Domain/Model/Key/KeyId.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SocialNetworksPublisher\Domain\Model\Key;
+
+class KeyId {
+    public const PREFIX = "key_";
+    public function __construct(private string $id = "")
+    {
+        if (empty($id)) {
+            $this->id = uniqid(self::PREFIX);
+        }
+    }
+
+    public function equals(KeyId $otherKey): bool {
+        if($this->id !== $otherKey->id) {
+            return false;
+        }
+        return true;
+    }
+
+    public function __toString()
+    {
+        return $this->id;
+    }
+}
